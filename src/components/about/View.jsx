5b47e3a717c821fdf087c742a45e7b2edd9d68c1
didiraja <didiraja@hotmail.com>
2022-10-31 14:40:31 -0300

    refactor: 💡 clean repo

--- src/components/about/View.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Page from "../Page";
-import Header from "./Header";
-import Content from "./Content";
-
-export default function AboutView() {
-  return <Page header={<Header />} content={<Content />} />;
-}
